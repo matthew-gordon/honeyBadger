ce1b8b1ebc33c13097cf75f47c0c4031a16b201b
Kyle Gray <th33nd3r@gmail.com>
2016-12-10 16:11:50 -0700

    fixed errors in post/patch routes, they now work

--- routes/badges.js
+++ routes/badges.js
@@ -5,122 +5,141 @@
 const express = require('express');
 const knex = require('../knex');
 const route = express.Router();
-const { camelizeKeys, decamelizeKeys } = require('humps');
+const {
+    camelizeKeys,
+    decamelizeKeys
+} = require('humps');
 const boom = require('boom');
 
 //get all badges
-route.get('/badges', (req, res, next) =>{
-  knex('badges')
-  //TODO: depending we might want to change how its ordered
-  .orderBy('id')
-  .then((badges) => {
-    const camelBadges = camelizeKeys(badges);
-    res.set('content-type', 'application/json');
-    res.send(camelBadges);
-  })
-  .catch((err) => {
-    // TODO: Use boom to create a custom err
-    next(err);
-  });
+route.get('/badges', (req, res, next) => {
+    knex('badges')
+        //TODO: depending we might want to change how its ordered
+        .orderBy('id')
+        .then((badges) => {
+            const camelBadges = camelizeKeys(badges);
+            res.set('content-type', 'application/json');
+            res.send(camelBadges);
+        })
+        .catch((err) => {
+            // TODO: Use boom to create a custom err
+            next(err);
+        });
 });
 
 //get specific badges
 route.get('/badges/:id', (req, res, next) => {
-  if(isNaN(req.params.id)){
-    // TODO: Use boom to create a custom err
-    return next();
-  }
-  knex('badges')
-  .where('id', req.params.id)
-  .first()
-  .then((badge) => {
-    if(!badge){
-      // TODO: Use boom to create a custom err
-      return next();
+    if (isNaN(req.params.id)) {
+        // TODO: Use boom to create a custom err
+        return next();
     }
-    const camelBadge = camelizeKeys(badge);
-    res.set('content-type', 'application/json');
-    res.send(camelBadge);
-  })
-  .catch((err) => {
-    // TODO: Use boom to create a custom err
-    next(err);
-  });
+    knex('badges')
+        .where('id', req.params.id)
+        .first()
+        .then((badge) => {
+            if (!badge) {
+                // TODO: Use boom to create a custom err
+                return next();
+            }
+            const camelBadge = camelizeKeys(badge);
+            res.set('content-type', 'application/json');
+            res.send(camelBadge);
+        })
+        .catch((err) => {
+            // TODO: Use boom to create a custom err
+            next(err);
+        });
 });
 
 // make a new badge!
 route.post('/badges', (req, res, next) => {
-  const decamelBadges = decamelizeKeys(req.body);
+    const decamelBadges = decamelizeKeys(req.body);
+    console.log(decamelBadges);
+    knex('badges')
+        .where('name', req.body.name)
+        .first()
+        .then((results) => {
+            if (!results) {
+                knex('badges')
+                    .insert({
+                        name: decamelBadges.name,
+                        badge_image_id: decamelBadges.badge_image_id,
+                        is_complete: decamelBadges.is_complete
+                    }, '*')
+                    .then(() => {
+                        knex('badges')
+                            .select('id', 'name', 'badge_image_id', 'is_complete')
+                            .where('name', req.body.name)
+                            .then(result => {
+                                res.send(camelizeKeys(result[0]));
+                            })
+                            .catch((err) => {
+                                // TODO: Use boom to create a custom err
+                                next(err);
+                            });
+                    })
+                    .catch((err) => {
+                        // TODO: Use boom to create a custom err
+                        next(err);
 
-  const badgeObj = {
-    name: decamelBadges.name,
-    badge_image_id: decamelBadges.badge_image_id,
-    badge_images: decamelBadges.badge_images,
-    is_complete: decamelBadges.is_complete
-  };
-  knex('badges')
-  .insert(badgeObj, '*')
-  .then((badge) => {
-    res.set('Content-Type', 'application/json');
-    res.send(camelizeKeys(badge[0]));
-  })
-  .catch((err) => {
-    // TODO: Use boom to create a custom err
-    next(err);
-  });
+                    });
+
+            } else {
+                res.status(400).send('User already exists');
+            }
+        });
 });
 
 //update one badge!
 route.patch('/badges/:id', (req, res, next) => {
-  const decamelBadges = decamelizeKeys(req.body);
-  knex('badges')
-  .where('id', req.params.id)
-  .first()
-  .then((badge) => {
-    if(!badge){
-      // TODO: Use boom to create a custom err
-      return next();
-    }
-    return knex('badges')
-    .update({
-      name: decamelBadges.name,
-      badge_image_id: decamelBadges.badge_image_id,
-      badge_images: decamelBadges.badge_images,
-      is_complete: decamelBadges.is_complete
-    }, '*')
-    .where('id', req.params.id);
-  })
-  .then((badge) => {
-    res.send(camelizeKeys(badge[0]));
-  })
-  .catch((err) => {
-    next(boom.create(404, "Not Found"));
-  });
+    const decamelBadges = decamelizeKeys(req.body);
+    knex('badges')
+        .where('id', req.params.id)
+        .first()
+        .then((badge) => {
+            if (!badge) {
+                // TODO: Use boom to create a custom err
+                return next();
+            }
+            return knex('badges')
+                .update({
+                    name: decamelBadges.name,
+                    badge_image_id: decamelBadges.badge_image_id,
+                    is_complete: decamelBadges.is_complete
+                }, '*')
+                .where('id', req.params.id);
+        })
+        .then((badge) => {
+            res.send(camelizeKeys(badge[0]));
+        })
+        .catch((err) => {
+            next(boom.create(404, "Not Found"));
+        });
 });
 
 //delete a badge
 route.delete('/badges/:id', (req, res, next) => {
-  let badge;
-  knex('badges')
-  .where('id', req.params.id)
-  .first()
-  .then((row) => {
-    if(!row) {
-      // TODO: Use boom to create a custom err
-      return next();
-    }
-    badge = row;
-    return knex('badges')
-    .del()
-    .where('id', req.params.id);
-  })
-  .then(() => {
-    delete badge.id;
-    res.send(camelizeKeys(badge));
-  })
-  .catch((err) => {
-    next(boom.create(404, "Not Found"));
-  });
+    let badge;
+    knex('badges')
+        .where('id', req.params.id)
+        .first()
+        .then((row) => {
+            if (!row) {
+                // TODO: Use boom to create a custom err
+                return next();
+            }
+            badge = row;
+            return knex('badges')
+                .del()
+                .where('id', req.params.id);
+        })
+        .then(() => {
+            delete badge.id;
+            res.send(camelizeKeys(badge));
+        })
+        .catch((err) => {
+            next(boom.create(404, "Not Found"));
+        });
 });
 
 module.exports = route;
--- routes/users.js
+++ routes/users.js
@@ -5,136 +5,138 @@ const bcrypt = require('bcrypt');
 const knex = require('../knex');
 const boom = require('boom');
 const {
-  camelizeKeys,
-  decamelizeKeys
-  } = require('humps');
+    camelizeKeys,
+    decamelizeKeys
+} = require('humps');
 const route = express.Router();
 
 //route to get all user information
 route.get('/users', (req, res, next) => {
-  knex('users')
-  .orderBy('id')
-  .then((users) => {
-    const camelUsers = camelizeKeys(users);
-    res.set('content-type', 'application/json');
-    res.send(camelUsers);
-  })
-  .catch((err) => {
-    // TODO: Use boom to create a custom err
-    next(err);
-  });
+    knex('users')
+        .orderBy('id')
+        .then((users) => {
+            const camelUsers = camelizeKeys(users);
+            res.set('content-type', 'application/json');
+            res.send(camelUsers);
+        })
+        .catch((err) => {
+            // TODO: Use boom to create a custom err
+            next(err);
+        });
 });
 
 //route to get a specfic users
-route.get('/users/:id', (req, res, next) =>{
-  //TODO: find max id and make sure `${id}` < max id in db
-  if(isNaN(req.params.id)) {
-    //TODO:use boom to create a custom error
-    return next();
-  }
-  knex('users')
-  .where('id', req.params.id)
-  .first()
-  .then((user) => {
-    if(!user){
-      // TODO: Use boom to create a custom err
-      return next();
+route.get('/users/:id', (req, res, next) => {
+    //TODO: find max id and make sure `${id}` < max id in db
+    if (isNaN(req.params.id)) {
+        //TODO:use boom to create a custom error
+        return next();
     }
-    const camelUser = camelizeKeys(user);
-    res.set('content-type', 'application/json');
-    res.send(camelUser);
-  })
-  .catch((err)=> {
-    // TODO: Use boom to create a custom err
-    next(err);
-  });
+    knex('users')
+        .where('id', req.params.id)
+        .first()
+        .then((user) => {
+            if (!user) {
+                // TODO: Use boom to create a custom err
+                return next();
+            }
+            const camelUser = camelizeKeys(user);
+            res.set('content-type', 'application/json');
+            res.send(camelUser);
+        })
+        .catch((err) => {
+            // TODO: Use boom to create a custom err
+            next(err);
+        });
 });
 
 //route to add an user with a hashed password
-route.post('/users', (req, res, next) =>{
-  var hash = bcrypt.hashSync(req.body.password, 8);
-  knex('users')
-  .where('email', req.body.email)
-  .first()
-  .then((results) =>{
-    if(!results){
-      knex('users')
-      .insert({
-        name: req.body.name,
-        email: req.body.email,
-        isAdmin: req.body.isAdmin
-      }, '*')
-      .then(()=>{
-        knex('users')
-        .select('id', 'name', 'email', 'isAdmin')
-        //TODO: this could also change to user name or however you login through github
+route.post('/users', (req, res, next) => {
+    console.log(req.body);
+    const decamelBadges = decamelizeKeys(req.body);
+    // var hash = bcrypt.hashSync(req.body.password, 8);
+    knex('users')
         .where('email', req.body.email)
-        .then(result => {
-          res.send(camelizeKeys(result[0]));
-        })
-        .catch((err) => {
-          // TODO: Use boom to create a custom err
-          next(err);
+        .first()
+        .then((results) => {
+            if (!results) {
+                knex('users')
+                    .insert({
+                        email: decamelBadges.email,
+                        is_admin: decamelBadges.is_admin,
+                        name: decamelBadges.name
+                    }, '*')
+                    .then(() => {
+                        knex('users')
+                            .select('id', 'email', 'isAdmin', 'name')
+                            //TODO: this could also change to user name or however you login through github
+                            .where('email', req.body.email)
+                            .then(result => {
+                                res.send(camelizeKeys(result[0]));
+                            })
+                            .catch((err) => {
+                                // TODO: Use boom to create a custom err
+                                next(err);
+                            });
+                    })
+                    .catch((err) => {
+                        // TODO: Use boom to create a custom err
+                        next(err);
+                    });
+            } else {
+                res.status(400).send('User already exists');
+            }
         });
-      })
-      .catch((err) =>{
-        // TODO: Use boom to create a custom err
-        next(err);
-      });
-    } else {
-      res.status(400).send('User already exists');
-    }
-  });
 });
 
 //Update one user!!!
 route.patch('/users/:id', (req, res, next) => {
-  const decamelUsers =decamelizeKeys(req.body);
-  knex('users')
-  .where('id', req.params.id)
-  .first()
-  .then((user) =>{
-    if(!user){
-      // TODO: Use boom to create a custom err
-      next();
-    }
-    return knex('users')
-    .update({
-      name: decamelUsers.name,
-      email: decamelUsers.email,
-      isAdmin: decamelUsers.isAdmin
-    }, '*')
-    .where('id', req.params.id);
-  })
-  .then((users) => {
-    res.send(camelizeKeys(users[0]));
-  })
-  .catch((err) => {
-    next(boom.create(404, "Not Found"));
-  });
+    const decamelUsers = decamelizeKeys(req.body);
+    knex('users')
+        .where('id', req.params.id)
+        .first()
+        .then((user) => {
+            if (!user) {
+                // TODO: Use boom to create a custom err
+                next();
+            }
+            return knex('users')
+                .update({
+                    name: decamelUsers.name,
+                    email: decamelUsers.email,
+                    is_admin: decamelUsers.isAdmin
+                }, '*')
+                .where('id', req.params.id);
+        })
+        .then((users) => {
+            res.send(camelizeKeys(users[0]));
+        })
+        .catch((err) => {
+            next(boom.create(404, "Not Found"));
+        });
 });
 
-route.delete('/users/:id', (req, res, next) =>{
-  let user;
-  knex('users')
-  .where('id', req.params.id)
-  .first()
-    .then((row) => {
-      if(!row) {
-        return next();
-      }
-      user = row;
-      return knex('users')
-      .del()
-      .where('id', req.params.id);
-    })
-      .then(() => {
-      delete user.id;
-      res.send(camelizeKeys(user));
-  })
-    .catch((err) => {
-      next(boom.create(404, "Not Found"));
-    });
-  });
+route.delete('/users/:id', (req, res, next) => {
+    let user;
+    knex('users')
+        .where('id', req.params.id)
+        .first()
+        .then((row) => {
+            if (!row) {
+                return next();
+            }
+            user = row;
+            return knex('users')
+                .del()
+                .where('id', req.params.id);
+        })
+        .then(() => {
+            delete user.id;
+            res.send(camelizeKeys(user));
+        })
+        .catch((err) => {
+            next(boom.create(404, "Not Found"));
+        });
+});
 
 module.exports = route;
