b0d344aa6bc1d30fc0ee758a32dc8214f9052b7d
CHANSE HORTON <chansehorton@yahoo.com>
2016-12-10 13:55:04 -0700

    updating seed files - these will give a tiny bit of (wrong) data to work with :badger:

--- seeds/04_users_badges.js
+++ seeds/04_users_badges.js
@@ -2,14 +2,18 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('users_badges').del()
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('table_name').insert({
+        knex('users_badges').insert({
           id: 1,
-          colName: 'rowValue1'
+          user_id: 2,
+          badge_id: 1
         })
       ]);
+    })
+    .then(function() {
+      return knex.raw("SELECT setval('users_badges_id_seq', (SELECT MAX(id) FROM users_badges))");
     });
 };
