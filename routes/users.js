eeb0f6fe96d5893befe082ff824116b7d0702f88
Kyle Gray <th33nd3r@gmail.com>
2016-12-10 12:05:35 -0700

    add patch to users routes

--- routes/users.js
+++ routes/users.js
@@ -88,7 +88,32 @@ route.post('/users', (req, res, next) =>{
     }
   });
 });
+//Update one user!!!
+route.patch('/users/:id', (req, res, next) => {
+  const decamelUsers =decamelizeKeys(req.body);
+  knex('users')
+  .where('id', req.params.id)
+  .first()
+  .then((user) =>{
+    if(!user){
+      // TODO: Use boom to create a custom err
+      next();
+    }
+    return knex('users')
+    .update({
+      // TODO: need to see db/migration before filling this out
 
+
+    })
+    .where('id', req.params.id);
+  })
+  .then((users) => {
+    res.send(camelizeKeys(users[0]));
+  })
+  .catch((err) => {
+    next(boom.create(404, "Not Found"));
+  });
+});
 
 route.delete('/users/:id', (req, res, next) =>{
   knex('users')
