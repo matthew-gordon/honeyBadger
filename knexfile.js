550e88a7279090337f38b57af07f43c9556983a9
Matthew Gordon <laxredemption5525@gmail.com>
2016-12-10 19:11:20 -0700

    Configured production database

--- knexfile.js
+++ knexfile.js
@@ -11,6 +11,11 @@ module.exports = {
   test: {
     client: 'pg',
     connection: 'postgres://localhost/honeyBadger_test'
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
 
 };
