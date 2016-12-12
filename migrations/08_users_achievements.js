762bf08d25e152aaaf61ae147b171058c3523ad5
CHANSE HORTON <chansehorton@yahoo.com>
2016-12-11 18:36:09 -0700

    normalized db language, ordered /badges return info, commented out achievements data for further review

--- migrations/08_users_achievements.js
+++ migrations/08_users_achievements.js
@@ -3,7 +3,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('users_achievements', (table) => {
     table.increments();
     table.integer('user_id').notNullable().references('id').inTable('users').onDelete('CASCADE');
-    table.integer('achievements_id').notNullable().references('id').inTable('achievements').onDelete('CASCADE');
+    table.integer('achievement_id').notNullable().references('id').inTable('achievements').onDelete('CASCADE');
     table.boolean('is_complete').notNullable().defaultTo(false);
   });
 };
--- seeds/08_users_achievements.js
+++ seeds/08_users_achievements.js
@@ -9,7 +9,7 @@ exports.seed = function(knex, Promise) {
         knex('users_achievements').insert({
           id: 1,
           user_id: 2,
-          achievements_id: 1,
+          achievement_id: 1,
           is_complete: false
         })
       ]);
