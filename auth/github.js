5ea01c57a080b5d0204685555ff258137149b13e
CHANSE HORTON <chansehorton@yahoo.com>
2016-12-12 13:20:35 -0700

    added underscore into process.env names

--- auth/github.js
+++ auth/github.js
@@ -7,9 +7,9 @@ require('dotenv').config();
 const init = require('./init');
 
 passport.use(new GitHubStrategy({
-  clientID: process.env.CLIENTID,
-  clientSecret: process.env.CLIENTSECRET,
-  callbackURL: process.env.CALLBACKURL
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: process.env.CALLBACK_URL
   },
  (accessToken, refreshToken, profile, done) => {
   console.log(profile);
