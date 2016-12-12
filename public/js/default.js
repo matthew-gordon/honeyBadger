64683f472103219225538bfaed984256dc27a079
Matthew Gordon <laxredemption5525@gmail.com>
2016-12-12 15:04:11 -0700

    Updated badges, added badge population rough

--- public/js/default.js
+++ public/js/default.js
@@ -1,22 +1,25 @@
 'use strict';
 
 $(document).ready(function() {
-const $badge_call = $('#badge_call');
-
-$badge_call.click(function(){
-  // console.log('click me Alice');
+// const $badge_call = $('#badge_call');
+const $badgeTest = $('#badgeTest');
 
   $.getJSON(`badges/2`)
-       .done((user) => {
-         console.log(user);
-       })
-       .fail(() => {
-         Materialize.toast('Unable to retrieve user', 3000);
-       });
-     })
+    .done((userBadges) => {
+        for ( var i = 0; i < userBadges.length; i++) {
+          if (userBadges[i].badgeComplete === true){
+            $badgeTest.append(
+              '<img src="' + userBadges[i].badgeCompleteLocation + '" class="badge badgeInactiveSmall">'
+            );
+          }
+        }
+    })
+    .fail(() => {
+     Materialize.toast('Unable to retrieve user', 3000);
+    });
 
-     //test
 
   // Initialize collapsable menu
   $(".button-collapse").sideNav();
+
 });
