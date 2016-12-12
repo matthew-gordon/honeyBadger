6ccede7af66e94254c7b59184feb110b203a9760
CHANSE HORTON <chansehorton@yahoo.com>
2016-12-12 12:08:52 -0700

    github login started

---
+++ auth/init.js
@@ -0,0 +1,17 @@
+'use strict';
+//this file still broken
+const passport = require('passport');
+
+
+module.exports = () => {
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+      done(err, user);
+    });
+  });
+
+};
