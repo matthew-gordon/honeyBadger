1035593fbd3e4833699414bd73a89f032e5c6dc6
CHANSE HORTON <chansehorton@yahoo.com>
2016-12-12 17:13:57 -0700

    changed success redirect route in auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -9,7 +9,7 @@ const passportGitHub = require('../auth/github');
 route.get('/auth/github', passportGitHub.authenticate('github', { scope: [ 'user: email' ] }));
 
 route.get('/auth/github/callback', passportGitHub.authenticate('github', { failureRedirect: '/login' }), (req, res) => {
-  res.redirect(`/badges/${req.user.github_id}`);
+  res.redirect(`/badges.html`);
 });
 
 module.exports = route;
