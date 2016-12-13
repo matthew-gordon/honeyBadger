d0e7283c0284e5bc2c7424c81d05a3fb2b9bbf81
Kyle Gray <th33nd3r@gmail.com>
2016-12-13 15:39:50 -0700

    added log out feature

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const route = express.Router();
-
+const authenticate = require('../auth/verification');
 const passportGitHub = require('../auth/github');
 
 
@@ -11,5 +11,13 @@ route.get('/auth/github', passportGitHub.authenticate('github', { scope: [ 'user
 route.get('/auth/github/callback', passportGitHub.authenticate('github', { failureRedirect: '/login' }), (req, res) => {
   res.redirect(`/badges.html`);
 });
+//allow user to logout maybe add a message telling them they have done so
+route.get('/log_out',
+  authenticate,
+  function(req, res, next) {
+  req.logout();
+  res.redirect('/login.html');
+});
+
 
 module.exports = route;
