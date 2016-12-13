6119d65b539f0f93570e27c97cf9d436b742deac
Matthew Gordon <laxredemption5525@gmail.com>
2016-12-12 17:52:43 -0700

    Updated badges JSON

--- public/js/default.js
+++ public/js/default.js
@@ -4,15 +4,22 @@ $(document).ready(function() {
 // const $badge_call = $('#badge_call');
 const $badgeTest = $('#badgeTest');
 
-  $.getJSON(`badges/${req.user.github_id}`)
-    .done((userBadges) => {
-        for ( var i = 0; i < userBadges.length; i++) {
-          if (userBadges[i].badgeComplete === true){
-            $badgeTest.append(
-              '<img src="' + userBadges[i].badgeCompleteLocation + '" class="badge badgeInactiveSmall">'
-            );
-          }
-        }
+  $.getJSON(`users/current`)
+    .done((user) => {
+        $.getJSON(`badges/${user.github_id}`)
+          .done((userBadges) => {
+            for ( var i = 0; i < userBadges.length; i++) {
+              if (userBadges[i].badgeComplete === false) {
+                $badgeTest.append(
+                  '<img src="' + userBadges[i].badgeCompleteLocation + '" class="badge badgeInactiveSmall">'
+                );
+              }
+            }
+          })
+          .fail(() => {
+            Materialize.toast('Unable to retrieve badges', 3000);
+          });
+        // console.log(user);
     })
     .fail(() => {
      Materialize.toast('Unable to retrieve user', 3000);
