ce7a84710e464b6c98441ecfd0005203c42f5807
Matthew Gordon <laxredemption5525@gmail.com>
2016-12-13 13:53:39 -0700

    Add user avatar to navbar

--- public/js/default.js
+++ public/js/default.js
@@ -34,4 +34,25 @@ const $badgeTest = $('#badgeTest');
   // Initialize collapsable menu
   $(".button-collapse").sideNav();
 
+  // Github chip
+  let $user = $('#user');
+
+    $.getJSON(`users/current`)
+      .done((user) => {
+          console.log(user.github_id);
+          console.log(user.gh_avatar_url);
+          console.log(user.name);
+
+          $user.append(
+            '<div class="chip">' +
+            '<img src="' + user.gh_avatar_url + '">' +
+            user.name +
+            '</div>'
+          );
+
+      })
+      .fail(() => {
+       Materialize.toast('Unable to retrieve user', 3000);
+      });
+
 });
