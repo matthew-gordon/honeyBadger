efd8770acd215780b24e7743689dd02aea79f1d2
Kyle Gray <th33nd3r@gmail.com>
2016-12-12 17:13:46 -0700

    create verification.js and intitialize it in the badges route

---
+++ auth/verification.js
@@ -0,0 +1,26 @@
+'use strict';
+
+
+
+function ensureAuthenticated(req, res, next) {
+  console.log(req);
+  // if (req.user) {
+  //   const userID = parseInt(req.user.id);
+    // userQueries.getSingleUserByID(userID, (err, user) => {
+    //   if (err) return next(err);
+    //   if (user.length && parseInt(user[0].id) === userID) {
+    //     return next();
+    //   } else {
+    //     return next('User does not exist.');
+    //   }
+    // });
+  // } else {
+  //   req.flash('messages', {
+  //     status: 'danger',
+  //     value: 'You need to sign in before continuing.'
+  //   });
+  //   return res.redirect('/auth/log_in');
+  // }
+}
+
+module.exports = ensureAuthenticated;
