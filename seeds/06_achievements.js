2c3a8ea272f21a12d699f1bcf3bf262b82e7f1a1
Kyle Gray <th33nd3r@gmail.com>
2016-12-14 19:15:52 -0700

    actually fixed db typos this time

--- seeds/06_achievements.js
+++ seeds/06_achievements.js
@@ -9,37 +9,37 @@ exports.seed = (knex, Promise) => {
         knex('achievements').insert({
           id: 1,
           name: 'You can make a website!',
-          image_url: 'public/img/htmlgold.svg',
+          image_url: '/img/htmlgold.svg',
           track_id: 1
         }),
         knex('achievements').insert({
           id: 2,
           name: 'You can make a better website!',
-          image_url: 'public/img/htmlgold.svg',
+          image_url: '/img/htmlgold.svg',
           track_id: 1
         }),
         knex('achievements').insert({
           id: 3,
           name: 'You can write a for loop!',
-          image_url: 'public/img/jsgold.svg',
+          image_url: '/img/jsgold.svg',
           track_id: 2
         }),
         knex('achievements').insert({
           id: 4,
           name: 'You can write constructor function!!',
-          image_url: 'public/img/jsgold.svg',
+          image_url: '/img/jsgold.svg',
           track_id: 2
         }),
         knex('achievements').insert({
           id: 5,
           name: 'You can make a database!',
-          image_url: 'public/img/dbgold.svg',
+          image_url: '/img/dbgold.svg',
           track_id: 3
         }),
         knex('achievements').insert({
           id: 6,
           name: 'You can use the crud app to talk to your database!',
-          image_url: 'public/img/dbgold.svg',
+          image_url: '/img/dbgold.svg',
           track_id: 3
         })
       ]);
