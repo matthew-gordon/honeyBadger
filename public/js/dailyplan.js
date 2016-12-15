feb43bd0c0e414dc36d0092596aa4ba74e2cc25b
Kyle Gray <th33nd3r@gmail.com>
2016-12-15 11:42:21 -0700

    fixed logo links and added person chip to daily plan page

--- public/js/dailyplan.js
+++ public/js/dailyplan.js
@@ -1,5 +1,36 @@
 'use strict';
 
+// Initialize collapsable menu
+$(".button-collapse").sideNav();
+
+// Github chip
+let $user = $('#user');
+let $naVatar = $('#naVatar');
+
+  $.getJSON(`users/current`)
+    .done((user) => {
+        console.log(user.github_id);
+        console.log(user.gh_avatar_url);
+        console.log(user.name);
+        $user.append(
+          '<div class="chip">' +
+          '<img src="' + user.gh_avatar_url + '">' +
+          user.name +
+          '</div>'
+        );
+
+        $naVatar.append(
+          '<div>' +
+          '<img class="avatarMedium" src="' + user.gh_avatar_url + '">' +
+          user.name +
+          '</div>'
+        );
+
+    })
+    .fail(() => {
+     Materialize.toast('Unable to retrieve user', 3000);
+    });
+
 const dailyPlan = $('#dailyPlan');
 console.log(dailyPlan);
 
